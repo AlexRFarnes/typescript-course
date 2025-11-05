f59cecd9085d0cc96ffa27af8f675c22e5c90970
AlexRFarnes <alexrfarnes@gmail.com>
2025-11-05 13:56:05 +0800

    11-section in-progress

--- 11-section/src/app.ts
+++ 11-section/src/app.ts
@@ -1,10 +1,29 @@
 // ************* Decorators *************
+
 function Logger(constructor: Function) {
   console.log("Logging...");
   console.log(constructor);
 }
 
-@Logger
+// ************* Decorator Factory *************
+function LoggerFactory(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+function WithTemplate(template: string, hookId: string) {
+  return function (_: Function) {
+    const hookEl = document.getElementById(hookId);
+    if (hookEl) {
+      hookEl.innerHTML = template;
+    }
+  };
+}
+
+// @LoggerFactory("LOGGING - PERSON")
+@WithTemplate("<h1>Hello World</h1>", "app")
 class Person {
   name = "Alex";
 
